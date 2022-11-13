49673aebe0eafb91799fe6f559bc5f88d8adcf96
paulmotyavin <isip_p.d.motyavin@mpt.ru>
2022-11-13 23:40:00 +0300

    доделываю вс-пн

---
+++ Converter/Clothes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    public class Clothes
+    {
+        public string Name;
+        public int Size;
+        public int Price;
+        public Clothes()
+        {
+
+        }
+
+        public Clothes(string name, int size, int price)
+        {
+            Name = name;
+            Size = size;
+            Price = price;
+            size.ToString();
+            price.ToString();
+        }
+    }
+}
---
+++ Converter/Program.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text.Json.Serialization;
+
+namespace Converter
+{
+    internal class Program
+    {
+        static public string open;
+        static void Main()
+        {
+            List<Clothes> file = new();
+            Clothes firstvariant = new("Футболка", 48, 699);
+            Clothes secondvariant = new("Джинсы", 30, 4500);
+            Clothes thirdvariant = new("Кроссовки", 42, 5200);
+            file.Add(firstvariant);
+            file.Add(secondvariant);
+            file.Add(thirdvariant);
+            Console.WriteLine("Введите путь до файла (с названием), который вы хотите открыть");
+            Console.WriteLine("--------------------------------------------------------------");
+            string first_path = Console.ReadLine();
+            FileInfo path = new FileInfo(first_path);
+            string txt = JsonConvert.DeserializeObject(file);
+                if (File.Exists(first_path))
+                {
+                    open = File.ReadAllText(first_path);
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Данного пути не существует, попробуйте снова");
+                    first_path = Console.ReadLine();
+                }
+            Console.Clear();
+            Console.WriteLine("Нажмите F1, чтобы сохранить файл в одном из трех расширений(txt, json, xml). Для выхода нажмите Escape");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------");
+            open = File.ReadAllText(first_path);
+            Console.WriteLine(open);
+            ConsoleKeyInfo button = Console.ReadKey();
+            if (button.Key == ConsoleKey.F1)
+            {
+                Console.WriteLine("Введите путь до файла (с названием), куда вы хотите сохранить текст");
+                Console.WriteLine("-------------------------------------------------------------------");
+                string second_path = Console.ReadLine();
+                if (File.Exists(first_path))
+                {
+                    open = File.ReadAllText(first_path);
+
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+    }
+}
+
+
+
