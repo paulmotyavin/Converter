41a934bc2ba16edf7bcee367440282c05983ed59
paulmotyavin <isip_p.d.motyavin@mpt.ru>
2022-12-05 19:56:02 +0300

    ура

--- Converter/Converter.cs
+++ Converter/Converter.cs
@@ -18,31 +18,26 @@ namespace Converter
         public static int price3;
         static public string open;
         static public List<Clothes> list = new List<Clothes>();
-        static private FileInfo path;
+        static public FileInfo path;
+        static public FileInfo path2;
         static public string first_path;
-
+        static public string second_path;
+        static public List<Clothes> clothes;
+        static public List<Clothes> file;
         static public void MainFunction()
         {
             Console.WriteLine("Введите путь до файла (с названием), который вы хотите открыть");
             Console.WriteLine("--------------------------------------------------------------");
             first_path = Console.ReadLine();
             path = new FileInfo(first_path);
-
             if (File.Exists(first_path))
             {
                 Console.Clear();
                 Console.WriteLine("Нажмите F1, чтобы сохранить файл в одном из трех расширений(txt, json, xml). Для выхода нажмите Escape");
                 Console.WriteLine("------------------------------------------------------------------------------------------------------");
-                if (path.Extension == ".txt")
-                {
-                    Read();
-                }
+                Read();
                 ConsoleKeyInfo button = Console.ReadKey();
-                if (button.Key == ConsoleKey.Escape) Process.GetCurrentProcess().Kill(); ;
-                if (button.Key == ConsoleKey.F1)
-                {
-                    Save();
-                }
+                if (button.Key == ConsoleKey.Escape) Process.GetCurrentProcess().Kill();
             }
             else
             {
@@ -57,104 +52,93 @@ namespace Converter
             Console.Clear();
             Console.WriteLine("Нажмите F1, чтобы сохранить файл в одном из трех расширений(txt, json, xml). Для выхода нажмите Escape");
             Console.WriteLine("------------------------------------------------------------------------------------------------------");
+            ReadTxt();
+            ReadJson();
+            ReadXml();
+        }
+        public static void Save()
+        {
+            Console.Clear();
+            Console.WriteLine("Введите путь до файла (с названием), куда вы хотите сохранить текст");
+            Console.WriteLine("-------------------------------------------------------------------");
+            second_path = Console.ReadLine();
+            path2 = new FileInfo(second_path);
+            SaveTxt();
+            SaveJson();
+            SaveXml();
+            
+            
+            Console.Clear();
+            Console.WriteLine("Файл был успешно конвертирован.");
+        }
+        private static void ReadTxt()
+        {
             if (path.Extension == ".txt")
             {
                 open = File.ReadAllText(first_path);
-                EditingText.Edit();
-                string[] words = open.Split("\n");
-                List<int> list1 = new List<int>();
-                List<string> list2 = new List<string>();
-                foreach (var word in words)
-                {
-                    int n;
-                    bool Check = int.TryParse(word, out n);
-                    if (Check == true) list1.Add(Convert.ToInt32(word));
-                    else list2.Add(word.TrimEnd());
-                }
-                name1 = list2[0];
-                name2 = list2[1];
-                name3 = list2[2];
-                size1 = list1[0];
-                size2 = list1[2];
-                size3 = list1[4];
-                price1 = list1[1];
-                price2 = list1[3];
-                price3 = list1[5];
-
-                Clothes firstvariant = new(name1, size1, price1);
-                Clothes secondvariant = new(name2, size2, price2);
-                Clothes thirdvariant = new(name3, size3, price3);
-
-                list.Add(firstvariant);
-                list.Add(secondvariant);
-                list.Add(thirdvariant);
-
-                foreach (Clothes item in list)
-                {
-                    Console.WriteLine(item.Name);
-                    Console.WriteLine(item.Size);
-                    Console.WriteLine(item.Price);
-                }
+                EditingText.EditTxt();
             }
+        }
+        private static void ReadJson() 
+        {
             if (path.Extension == ".json")
             {
                 open = File.ReadAllText(first_path);
-                List<Clothes> file = JsonConvert.DeserializeObject<List<Clothes>>(open);
-                foreach (var item in file)
-                {
-                    Console.WriteLine(item.Name);
-                    Console.WriteLine(item.Size);
-                    Console.WriteLine(item.Price);
-                }
-                list = file;
+                file = JsonConvert.DeserializeObject<List<Clothes>>(open);
+                EditingText.Json();
             }
+        }
+        private static void ReadXml()
+        {
             if (path.Extension == ".xml")
             {
-                List<Clothes> clothes;
 
                 XmlSerializer xml = new XmlSerializer(typeof(List<Clothes>));
                 using (FileStream fs = new FileStream(first_path, FileMode.Open))
                 {
                     clothes = (List<Clothes>)xml.Deserialize(fs);
                 }
-                foreach (var item in clothes)
-                {
-                    Console.WriteLine(item.Name);
-                    Console.WriteLine(item.Size);
-                    Console.WriteLine(item.Price);
-                }
-                list = clothes;
+                EditingText.EditXml();
             }
         }
-        private static void Save()
+        private static void SaveTxt()
         {
-            Console.Clear();
-            Console.WriteLine("Введите путь до файла (с названием), куда вы хотите сохранить текст");
-            Console.WriteLine("-------------------------------------------------------------------");
-            string second_path = Console.ReadLine();
-            FileInfo path2 = new FileInfo(second_path);
             if (path2.Extension == ".txt")
             {
-                if (path.Extension == ".txt")
+                SaveTxtToTxt();
+                SaveTxtToJsonXml();
+                
+            }
+        }
+        private static void SaveTxtToTxt()
+        {
+            if (path.Extension == ".txt")
+            {
+                File.WriteAllText(second_path, open);
+            }
+        }
+        private static void SaveTxtToJsonXml()
+        {
+            if (path.Extension == ".json" || path.Extension == ".xml")
+            {
+                foreach (var item in list)
                 {
-                    File.WriteAllText(second_path, open);
-                }
-                else
-                {
-                    foreach (var item in list)
-                    {
-                        File.AppendAllText(second_path, item.Name + "\n");
-                        File.AppendAllText(second_path, item.Price.ToString() + "\n");
-                        File.AppendAllText(second_path, item.Size.ToString() + "\n");
-                    }
-
+                    File.AppendAllText(second_path, item.Name + "\n");
+                    File.AppendAllText(second_path, item.Price.ToString() + "\n");
+                    File.AppendAllText(second_path, item.Size.ToString() + "\n");
                 }
             }
+        }
+        private static void SaveJson()
+        {
             if (path2.Extension == ".json")
             {
                 string json = JsonConvert.SerializeObject(list);
                 File.WriteAllText(second_path, json);
             }
+        }
+        private static void SaveXml()
+        {
             if (path2.Extension == ".xml")
             {
                 XmlSerializer xml = new XmlSerializer(typeof(List<Clothes>));
@@ -164,8 +148,7 @@ namespace Converter
                 }
 
             }
-            Console.Clear();
-            Console.WriteLine("Файл был успешно конвертирован.");
         }
+
     }
 }
--- Converter/EditingText.cs
+++ Converter/EditingText.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace Converter
 {
     public class EditingText
     {
-        public static void Edit()
+        public static List<string> name;
+        public static List<char> chars = new List<char>();
+        public static string open;
+        public static string[] words;
+        public static List<Clothes> parseList= new List<Clothes>();
+        public static List<string> jsonXml = new List<string>();
+        public static void EditTxt()
         {
-
-            List<char> chars = Converter.open.ToCharArray().ToList();
-            Console.WriteLine(Converter.open);
+            open = Converter.open;
+            words = open.Split('\n');
 
             ConsoleKeyInfo button;
             int pos = 0;
-            int pos1 = 2;
-            Console.SetCursorPosition(0, 2);
+            int pos1 = 0;
             do
             {
+                chars = words[pos1].ToString().ToCharArray().ToList();
                 button = Console.ReadKey();
                 if (button.Key == ConsoleKey.LeftArrow)
                 {
@@ -29,35 +33,230 @@ namespace Converter
                 {
                     pos = (pos + 1 == chars.Count + 1) ? pos : pos + 1;
                 }
-                if (button.Key == ConsoleKey.LeftArrow)
+                if (button.Key == ConsoleKey.DownArrow)
                 {
-                    pos1 = (pos1 - 1 == -1) ? pos1 : pos1 - 1;
+                    pos1++;
+                    chars = words[pos1].ToString().ToCharArray().ToList();
                 }
-                if (button.Key == ConsoleKey.LeftArrow)
+                if (button.Key == ConsoleKey.UpArrow)
                 {
-                    pos1 = (pos1 - 1 == -1) ? pos1 : pos1 + 1;
+                    pos1--;
+                    chars = words[pos1].ToString().ToCharArray().ToList();
                 }
                 else if (button.Key == ConsoleKey.Backspace)
                 {
                     chars.RemoveAt(pos - 1);
                     pos--;
                 }
-                else if (Char.IsLetterOrDigit(button.KeyChar) || Char.IsWhiteSpace(button.KeyChar))
+                if (button.Key == ConsoleKey.Spacebar)
+                {
+                    chars.Insert(pos, ' ');
+                    pos++;
+                }
+                else if (Char.IsLetterOrDigit(button.KeyChar))
+                {
+                    chars.Insert(pos, button.KeyChar);
+                    pos++;
+
+                }
+                words[pos1] = string.Join("", chars);
+                chars = words[pos1].ToString().ToCharArray().ToList();
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("                                  ");
+                Console.SetCursorPosition(0, 0);
+                foreach (var item in words)
+                    Console.WriteLine(item);
+                Console.SetCursorPosition(pos, pos1);                
+            } while (button.Key != ConsoleKey.F1);
+            ParseForConstructorTxt();
+            Converter.Save();
+        }
+        public static void Json()
+        {
+            foreach (var i in Converter.file)
+            {
+                jsonXml.Add(i.Name.ToString());
+                jsonXml.Add(i.Size.ToString());
+                jsonXml.Add(i.Price.ToString());
+            }
+            ConsoleKeyInfo button;
+            int pos = 0;
+            int pos1 = 0;
+            do
+            {
+                chars = jsonXml[pos1].ToString().ToCharArray().ToList();
+                button = Console.ReadKey();
+                if (button.Key == ConsoleKey.LeftArrow)
+                {
+                    pos = (pos - 1 == -1) ? pos : pos - 1;
+                }
+                else if (button.Key == ConsoleKey.RightArrow)
+                {
+                    pos = (pos + 1 == chars.Count + 1) ? pos : pos + 1;
+                }
+                if (button.Key == ConsoleKey.DownArrow)
+                {
+                    pos1++;
+                    chars = jsonXml[pos1].ToString().ToCharArray().ToList();
+                }
+                if (button.Key == ConsoleKey.UpArrow)
+                {
+                    pos1--;
+                    chars = jsonXml[pos1].ToString().ToCharArray().ToList();
+                }
+                else if (button.Key == ConsoleKey.Backspace)
+                {
+                    chars.RemoveAt(pos - 1);
+                    pos--;
+                }
+                if (button.Key == ConsoleKey.Spacebar)
+                {
+                    chars.Insert(pos, ' ');
+                    pos++;
+                }
+                else if (Char.IsLetterOrDigit(button.KeyChar))
                 {
                     chars.Insert(pos, button.KeyChar);
                     pos++;
                 }
-                Console.SetCursorPosition(0, 2);
+                jsonXml[pos1] = string.Join("", chars);
+                chars = jsonXml[pos1].ToString().ToCharArray().ToList();
+                Console.SetCursorPosition(0, 0);
                 Console.WriteLine("                                  ");
-                Console.SetCursorPosition(0, 2);
-                foreach (var item in chars)
-                {
-                    Console.Write(item);
-
-                }
-
-                Console.SetCursorPosition(pos, 2);
+                Console.SetCursorPosition(0, 0);
+                foreach (var item in jsonXml)
+                    Console.WriteLine(item);
+                Console.SetCursorPosition(pos, pos1);
             } while (button.Key != ConsoleKey.F1);
+            ParseForConstructorXmlJson();
+            Converter.Save();
+        }
+        public static void EditXml()
+        {
+            foreach (var i in Converter.clothes)
+            {
+                jsonXml.Add(i.Name.ToString());
+                jsonXml.Add(i.Size.ToString());
+                jsonXml.Add(i.Price.ToString());
+            }
+            ConsoleKeyInfo button;
+            int pos = 0;
+            int pos1 = 0;
+            do
+            {
+                chars = jsonXml[pos1].ToString().ToCharArray().ToList();
+                button = Console.ReadKey();
+                if (button.Key == ConsoleKey.LeftArrow)
+                {
+                    pos = (pos - 1 == -1) ? pos : pos - 1;
+                }
+                else if (button.Key == ConsoleKey.RightArrow)
+                {
+                    pos = (pos + 1 == chars.Count + 1) ? pos : pos + 1;
+                }
+                if (button.Key == ConsoleKey.DownArrow)
+                {
+                    pos1++;
+                    chars = jsonXml[pos1].ToString().ToCharArray().ToList();
+                }
+                if (button.Key == ConsoleKey.UpArrow)
+                {
+                    pos1--;
+                    chars = jsonXml[pos1].ToString().ToCharArray().ToList();
+                }
+                else if (button.Key == ConsoleKey.Backspace)
+                {
+                    chars.RemoveAt(pos - 1);
+                    pos--;
+                }
+                if (button.Key == ConsoleKey.Spacebar)
+                {
+                    chars.Insert(pos, ' ');
+                    pos++;
+                }
+                else if (Char.IsLetterOrDigit(button.KeyChar))
+                {
+                    chars.Insert(pos, button.KeyChar);
+                    pos++;
+                }
+                jsonXml[pos1] = string.Join("", chars);
+                chars = jsonXml[pos1].ToString().ToCharArray().ToList();
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("                                  ");
+                Console.SetCursorPosition(0, 0);
+                foreach (var item in jsonXml)
+                    Console.WriteLine(item);
+                Console.SetCursorPosition(pos, pos1);
+            } while (button.Key != ConsoleKey.F1);
+            ParseForConstructorXmlJson();
+            Converter.Save();
+        }
+        
+        public static void ParseForConstructorTxt()
+        {
+            List<int> list1 = new List<int>();
+            List<string> list2 = new List<string>();
+            foreach (var word in words)
+            {
+                int n;
+                bool Check = int.TryParse(word, out n);
+                if (Check == true) list1.Add(Convert.ToInt32(word));
+                else list2.Add(word.TrimEnd());
+            }
+            Converter.name1 = list2[0];
+            Converter.name2 = list2[1];
+            Converter.name3 = list2[2];
+            Converter.size1 = list1[0];
+            Converter.size2 = list1[2];
+            Converter.size3 = list1[4];
+            Converter.price1 = list1[1];
+            Converter.price2 = list1[3];
+            Converter.price3 = list1[5];
+
+            Clothes firstvariant = new(Converter.name1, Converter.size1, Converter.price1);
+            Clothes secondvariant = new(Converter.name2, Converter.size2, Converter.price2);
+            Clothes thirdvariant = new(Converter.name3, Converter.size3, Converter.price3);
+
+            Converter.list.Add(firstvariant);
+            Converter.list.Add(secondvariant);
+            Converter.list.Add(thirdvariant);
+
+            foreach (Clothes item in Converter.list)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Size);
+                Console.WriteLine(item.Price);
+            }
+        }
+        public static void ParseForConstructorXmlJson()
+        {
+            List<int> list1 = new List<int>();
+            List<string> list2 = new List<string>();
+            foreach (var cloth in jsonXml)
+            {
+                int n;
+                bool Check = int.TryParse(cloth, out n);
+                if (Check == true) list1.Add(Convert.ToInt32(cloth));
+                else list2.Add(cloth.TrimEnd());
+            }
+
+            Converter.name1 = list2[0];
+            Converter.name2 = list2[1];
+            Converter.name3 = list2[2];
+            Converter.size1 = list1[0];
+            Converter.size2 = list1[2];
+            Converter.size3 = list1[4];
+            Converter.price1 = list1[1];
+            Converter.price2 = list1[3];
+            Converter.price3 = list1[5];
+
+            Clothes firstvariant = new(Converter.name1, Converter.size1, Converter.price1);
+            Clothes secondvariant = new(Converter.name2, Converter.size2, Converter.price2);
+            Clothes thirdvariant = new(Converter.name3, Converter.size3, Converter.price3);
+
+            Converter.list.Add(firstvariant);
+            Converter.list.Add(secondvariant);
+            Converter.list.Add(thirdvariant);
         }
     }
 }
